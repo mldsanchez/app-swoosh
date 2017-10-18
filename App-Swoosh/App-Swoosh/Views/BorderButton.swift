3db66d6b138fc6bec663b8658709dac8e37e7856
dsanchez <MorseLife@ML-Dev-Macbook-Pro.local>
2017-10-17 23:41:45 -0400

    Started creating interface for Swoosh App

---
+++ App-Swoosh/App-Swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  App-Swoosh
+//
+//  Created by MorseLife on 10/17/17.
+//  Copyright © 2017 MorseLife. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
